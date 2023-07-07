ce3b005ef560ba162429a11ee85ce036a5378d32
david-fernando <davidfernandodamata21@gmail.com>
2023-07-07 18:46:00 -0300

    Update DigitalClock.js

--- src/components/DigitalClock.js
+++ src/components/DigitalClock.js
@@ -9,9 +9,6 @@ function DigitalClock(){
 
   return (
     <div className={styles.digitalClockContainer}>
-      <Textfit mode='single'>
-        <h1 className={styles.nextSignal}>NEXT SIGNAL</h1>
-      </Textfit>
       <span>
         <span className={styles.minutes}>
           <div>{time.minutes[0]}</div>
@@ -23,6 +20,9 @@ function DigitalClock(){
           <div>{time.seconds[1]}</div>
         </span>
       </span>
+      <Textfit mode='single'>
+        <h1 className={styles.nextSignal}>NEXT SIGNAL</h1>
+      </Textfit>
     </div>
   )
 }
