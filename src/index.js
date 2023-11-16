7058b4be7d2a2a5d5952ab4d63ff6b2d96e6db77
priemum <50713962+priemum@users.noreply.github.com>
2023-11-16 09:09:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { WebSocketProvider } from './context/webSocketContext'
 import { ModalProvider } from './context/modalContext'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));         
 root.render(
   <WebSocketProvider>
     <ModalProvider>
